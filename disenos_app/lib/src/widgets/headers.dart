a647100d1396a29e8ab80c036840b0e0ae292dda
CesarArellano <raywayday@gmail.com>
2021-05-09 12:50:24 -0500

    Problems with diagonal containers

--- disenos_app/lib/src/widgets/headers.dart
+++ disenos_app/lib/src/widgets/headers.dart
@@ -27,3 +27,14 @@ class RoundedBorderHeader extends StatelessWidget {
     );
   }
 }
+
+class DiagonalHeader extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xff615AAB),
+    );
+  }
+}
