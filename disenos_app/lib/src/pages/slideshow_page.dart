64dc0492c541687cdba44b5e1c7b0a1cb60697d9
CesarArellano <raywayday@gmail.com>
2021-05-20 18:26:25 -0500

    Using PageViewController

--- disenos_app/lib/src/pages/slideshow_page.dart
+++ disenos_app/lib/src/pages/slideshow_page.dart
@@ -18,46 +18,32 @@ class SlideShowPage extends StatelessWidget {
   }
 }
 
-class _Dots extends StatelessWidget {
+class _Slides extends StatefulWidget {
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          _Dot(),
-          _Dot(),
-          _Dot(),
-        ],
-      )
-    );
-  }
+  __SlidesState createState() => __SlidesState();
+}
+
+class __SlidesState extends State<_Slides> {
+  final pageViewController = new PageController();
   
-}
-
-class _Dot extends StatelessWidget {
-
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      width: 12,
-      height: 12,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.grey
-      ),
-    );
+  void initState() { 
+    super.initState();
+    pageViewController.addListener(() {
+      print('Current Page: ${ pageViewController.page }');
+    });
   }
-}
-
-class _Slides extends StatelessWidget {
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: pageViewController,
       physics: BouncingScrollPhysics(),
       children: <Widget>[
         _Slide(svg: 'assets/svgs/slide-1.svg'),
@@ -82,6 +68,43 @@ class _Slide extends StatelessWidget {
       height: double.infinity,
       padding: EdgeInsets.all(30),
       child: SvgPicture.asset(svg)
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _Dot(index: 0),
+          _Dot(index: 1),
+          _Dot(index: 2),
+        ],
+      )
+    );
+  }
+  
+}
+
+class _Dot extends StatelessWidget {
+  final int index;
+  _Dot({ this.index });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.grey
+      ),
     );
   }
 }
