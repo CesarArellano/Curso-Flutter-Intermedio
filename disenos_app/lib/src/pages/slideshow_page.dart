e029db3d652af91054cefe9a2e60b215730ba8c9
CesarArellano <raywayday@gmail.com>
2021-05-20 18:19:54 -0500

    Designing Slideshow's indicators

--- disenos_app/lib/src/pages/slideshow_page.dart
+++ disenos_app/lib/src/pages/slideshow_page.dart
@@ -7,8 +7,48 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _Slides(),
+        child: Column(
+          children: [
+            Expanded(child: _Slides()),
+            _Dots(),
+          ],
+        ),
       )
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _Dot(),
+          _Dot(),
+          _Dot(),
+        ],
+      )
+    );
+  }
+  
+}
+
+class _Dot extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.grey
+      ),
     );
   }
 }
@@ -17,17 +57,15 @@ class _Slides extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PageView(
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          _Slide(svg: 'assets/svgs/slide-1.svg'),
-          _Slide(svg: 'assets/svgs/slide-2.svg'),
-          _Slide(svg: 'assets/svgs/slide-3.svg'),
-          _Slide(svg: 'assets/svgs/slide-4.svg'),
-          _Slide(svg: 'assets/svgs/slide-5.svg'),
-        ],
-      ),
+    return PageView(
+      physics: BouncingScrollPhysics(),
+      children: <Widget>[
+        _Slide(svg: 'assets/svgs/slide-1.svg'),
+        _Slide(svg: 'assets/svgs/slide-2.svg'),
+        _Slide(svg: 'assets/svgs/slide-3.svg'),
+        _Slide(svg: 'assets/svgs/slide-4.svg'),
+        _Slide(svg: 'assets/svgs/slide-5.svg'),
+      ],
     );
   }
 }
