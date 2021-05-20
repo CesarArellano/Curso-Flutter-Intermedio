bc04fe5aa4efcb27f72e4c6956d9b5cdf9e3a2ce
CesarArellano <raywayday@gmail.com>
2021-05-20 18:03:07 -0500

    Creating project space to slideshow Widget

---
+++ disenos_app/lib/src/pages/slideshow_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SlideShowPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SvgPicture.asset("assets/svgs/slide-1.svg"),
+            SvgPicture.asset("assets/svgs/slide-2.svg"),
+            SvgPicture.asset("assets/svgs/slide-3.svg"),
+            SvgPicture.asset("assets/svgs/slide-4.svg"),
+            SvgPicture.asset("assets/svgs/slide-5.svg"),
+          ],
+        ),
+      )
+    );
+  }
+}
