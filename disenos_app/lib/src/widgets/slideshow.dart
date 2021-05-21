e7f17c8b1647ef1b30f72de9314ada71f28a4528
CesarArellano <raywayday@gmail.com>
2021-05-20 19:00:58 -0500

    Starting to optimize the widget

---
+++ disenos_app/lib/src/widgets/slideshow.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+
+import 'package:disenos_app/src/models/slider_model.dart';
+
+class SlideShow extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => new SliderModel(),
+      child: Center(
+        child: Column(
+          children: [
+            Expanded(child: _Slides()),
+            _Dots(),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class _Slides extends StatefulWidget {
+
+  @override
+  __SlidesState createState() => __SlidesState();
+}
+
+class __SlidesState extends State<_Slides> {
+  final pageViewController = new PageController();
+  
+  @override
+  void initState() { 
+    super.initState();
+    pageViewController.addListener(() {
+      // Updating currentPage value
+      Provider.of<SliderModel>(context, listen: false).currentPage = pageViewController.page;
+    });
+  }
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageViewController,
+      physics: BouncingScrollPhysics(),
+      children: <Widget>[
+        _Slide(svg: 'assets/svgs/slide-1.svg'),
+        _Slide(svg: 'assets/svgs/slide-2.svg'),
+        _Slide(svg: 'assets/svgs/slide-3.svg'),
+        _Slide(svg: 'assets/svgs/slide-4.svg'),
+        _Slide(svg: 'assets/svgs/slide-5.svg'),
+      ],
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  _Slide({this.svg});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30),
+      child: SvgPicture.asset(svg)
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _Dot(index: 0),
+          _Dot(index: 1),
+          _Dot(index: 2),
+          _Dot(index: 3),
+          _Dot(index: 4),
+        ],
+      )
+    );
+  }
+  
+}
+
+class _Dot extends StatelessWidget {
+  final int index;
+  _Dot({ this.index });
+  @override
+  Widget build(BuildContext context) {
+    final pageViewIndex = Provider.of<SliderModel>(context).currentPage;
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),      
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      width: 12,
+      height: 12,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: (pageViewIndex >= index - 0.5 && pageViewIndex < index + 0.5) 
+          ? Colors.blue 
+          : Colors.grey
+      ),
+    );
+  }
+}
