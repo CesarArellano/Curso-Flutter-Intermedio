04af2b378077187cca558e41c689895b457a3d1e
CesarArellano <raywayday@gmail.com>
2021-05-25 22:12:22 -0500

    Creating IconHeader Widget

--- disenos_app/lib/main.dart
+++ disenos_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:disenos_app/src/pages/pinterest_page.dart';
+import 'package:disenos_app/src/pages/emergency_page.dart';
+//import 'package:disenos_app/src/pages/pinterest_page.dart';
 //import 'package:disenos_app/src/pages/slideshow_page.dart';
 //import 'package:disenos_app/src/pages/pie_charts_page.dart';
 //import 'package:disenos_app/src/pages/circular_progress_page.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: PinterestPage()
+      home: EmergencyPage()
     );
   }
 }
--- disenos_app/lib/src/widgets/headers.dart
+++ disenos_app/lib/src/widgets/headers.dart
@@ -291,3 +291,24 @@ class _GradientHeaderPainter extends CustomPainter {
   }
 
 }
+
+class IconHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 300,      
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color> [
+            Color(0xff526BF6),
+            Color(0xff67ACF2)
+          ],
+        ),
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80))
+      ),
+    );
+  }
+}
