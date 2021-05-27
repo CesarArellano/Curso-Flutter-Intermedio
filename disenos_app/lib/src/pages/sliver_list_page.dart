8ea9285a7bc390734450a9e9b66a4caa9840af89
CesarArellano <raywayday@gmail.com>
2021-05-27 18:07:59 -0500

    Knowing CustomScrollView y Slivers

--- disenos_app/lib/src/pages/sliver_list_page.dart
+++ disenos_app/lib/src/pages/sliver_list_page.dart
@@ -6,12 +6,43 @@ class SliverListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _TaskList()
+      body: _MainScroll()
+    );
+  }
+}
+class _MainScroll extends StatelessWidget {
+  final items = [
+    _ListItem( 'Orange', Color(0xffF08F66) ),
+    _ListItem( 'Family', Color(0xffF2A38A) ),
+    _ListItem( 'Subscriptions', Colors.pinkAccent ),
+    _ListItem( 'Books', Color(0xffFCEBAF) ),
+    _ListItem( 'Orange', Color(0xffF08F66) ),
+    _ListItem( 'Family', Color(0xffF2A38A) ),
+    _ListItem( 'Subscriptions', Color(0xffF7CDD5) ),
+    _ListItem( 'Books', Color(0xffFCEBAF) ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: BouncingScrollPhysics(),
+      slivers: <Widget> [
+        SliverAppBar(
+          floating: true,
+          elevation: 0,
+          backgroundColor: Colors.red,
+          title: Text('Hola Mundo')
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(items),
+        )
+      ],
     );
   }
 }
 
 class _Title extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return Column(
