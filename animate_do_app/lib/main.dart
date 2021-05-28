d4aff95373e57fed9cd778b60122b81ddd6ac68a
CesarArellano <raywayday@gmail.com>
2021-05-27 19:01:06 -0500

    Starting Animate_Do

---
+++ animate_do_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:animate_do_app/src/pages/screen1_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Screen1Page()
+    );
+  }
+}
---
+++ animate_do_app/lib/src/pages/screen1_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class Screen1Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
