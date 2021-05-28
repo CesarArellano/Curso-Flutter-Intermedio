5e2f8a67d1ae3fc99fcdbef3105cf437a9dd87e8
CesarArellano <raywayday@gmail.com>
2021-05-27 19:21:35 -0500

    Knowing animations with animate_do

--- animate_do_app/lib/src/pages/screen1_page.dart
+++ animate_do_app/lib/src/pages/screen1_page.dart
@@ -1,13 +1,68 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animate_do/animate_do.dart';
 
 class Screen1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(        
+        title: FadeIn(
+          delay: Duration(milliseconds: 500),
+          child: Text('Animate Do')
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: (){},
+            icon: FaIcon(FontAwesomeIcons.twitter),
+          ),
+          SlideInLeft(
+            from: 50,
+            child: IconButton(
+              onPressed: (){
+                Navigator.push(context, CupertinoPageRoute(builder: (BuildContext context) => Screen1Page()));
+              },
+              icon: Icon(Icons.navigate_next),
+            ),
+          ),
+        ],
+      ),
       body: Center(
-        child: Text('Hola Mundo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElasticIn(
+              delay: Duration(milliseconds: 1100),
+              child: Icon(Icons.new_releases, color: Colors.blue, size: 40),
+            ),
+            FadeInDown(
+              delay: Duration(milliseconds: 200),
+              child: Text('Titulo', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w200))
+            ),
+            FadeInDown(
+              delay: Duration(milliseconds: 800),
+              child: Text('Soy un texto pequeño', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400))
+            ),
+            FadeInLeft(
+              delay: Duration(milliseconds: 1100),
+              child: Container(
+                width: 220,
+                height: 2,
+                color: Colors.blue
+              ),
+            )
+          ],
+        ),
+      ),
+
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          child: FaIcon(FontAwesomeIcons.play),
+          onPressed: () {},
+        ),
       ),
     );
   }
